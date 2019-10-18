a7791736e622d2e86d22f810645697bab3584d87
hiroki19990625 <hiroki19990625@gmail.com>
2019-10-18 21:27:29 +0900

    ダイアグラムを作成

---
+++ SysDev2019/DatabaseInstance.cs
@@ -0,0 +1,45 @@
+﻿using ObjectDatabase;
+using SysDev2019.DataModels;
+
+namespace SysDev2019
+{
+    public static class DatabaseInstance
+    {
+        public static readonly ObjectDatabase.ObjectDatabase Database;
+
+        public static readonly DataTable<Product> ProductTable;
+        public static readonly DataTable<Manufacturer> ManufacturerTable;
+        public static readonly DataTable<Employee> EmployeeTable;
+        public static readonly DataTable<Department> DepartmentTable;
+        public static readonly DataTable<Order> OrderTable;
+        public static readonly DataTable<Ordering> OrderingTable;
+        public static readonly DataTable<Stock> StockTable;
+
+        static DatabaseInstance()
+        {
+            Database = new ObjectDatabase.ObjectDatabase("SysDev2019.accdb");
+            ProductTable = new DataTable<Product>("商品");
+            ManufacturerTable = new DataTable<Manufacturer>("メーカー");
+            DepartmentTable = new DataTable<Department>("部署");
+            EmployeeTable = new DataTable<Employee>("社員");
+            OrderTable = new DataTable<Order>("受注");
+            OrderingTable = new DataTable<Ordering>("発注");
+            StockTable = new DataTable<Stock>("在庫");
+            Database.AddTable(ManufacturerTable);
+            Database.AddTable(ProductTable);
+            Database.AddTable(DepartmentTable);
+            Database.AddTable(EmployeeTable);
+            Database.AddTable(OrderingTable);
+            Database.AddTable(OrderTable);
+            Database.AddTable(StockTable);
+
+            ProductTable.Union(ManufacturerTable);
+            EmployeeTable.Union(DepartmentTable);
+            OrderTable.Union(EmployeeTable, "EmployeeId");
+            OrderTable.Union(ProductTable, "ProductId");
+            OrderingTable.Union(ProductTable, "ProductId");
+            OrderingTable.Union(EmployeeTable, "EmployeeId");
+            StockTable.Union(ProductTable);
+        }
+    }
+}
---
+++ SysDev2019/LoginForm.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SysDev2019
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+
+            var _ = DatabaseInstance.Database;
+        }
+
+        public bool Login(string employeeId, string password)
+        {
+            return DatabaseInstance.EmployeeTable.Where(e => e.EmployeeId == employeeId).FirstOrDefault()?.Password ==
+                   ToSha256(password);
+        }
+
+        public bool IsLogisticsManager(string employeeId)
+        {
+            return DatabaseInstance.EmployeeTable.Where(e => e.EmployeeId == employeeId).FirstOrDefault()
+                       ?.DepartmentId == "20";
+        }
+
+        public bool IsSalesStaff(string employeeId)
+        {
+            return DatabaseInstance.EmployeeTable.Where(e => e.EmployeeId == employeeId).FirstOrDefault()
+                       ?.DepartmentId == "10";
+        }
+
+        public void NextForm()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void InvalidAuthMessage()
+        {
+            ErrorMessage("ログインに失敗しました。");
+        }
+
+        public void ErrorMessage(string message)
+        {
+            MessageBox.Show(message, "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        public void NotPermissionMessage()
+        {
+            ErrorMessage("権限がありません。");
+        }
+
+        private string ToSha256(string password)
+        {
+            SHA256 sha256 = SHA256.Create();
+            sha256.Initialize();
+
+            Encoding encoding = Encoding.UTF8;
+            return encoding.GetString(sha256.ComputeHash(encoding.GetBytes(password)));
+        }
+    }
+}
