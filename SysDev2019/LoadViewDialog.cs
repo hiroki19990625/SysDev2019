fea99901da420189e4131ba072d2ab499dccfdae
hiroki19990625 <hiroki19990625@gmail.com>
2019-11-28 16:07:59 +0900

    進歩お知らせダイアログ

---
+++ SysDev2019/LoadViewDialog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SysDev2019
+{
+    public partial class LoadViewDialog : Form
+    {
+        public LoadViewDialog()
+        {
+            InitializeComponent();
+
+            progressBar1.MarqueeAnimationSpeed = 100;
+        }
+    }
+}
